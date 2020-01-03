8a27a8ad809c415f3c2dc4aa3d604f9443274671
千江水月 <499633983@qq.com>
2020-01-04 06:24:34 +0800

    油画

---
+++ eurakeserver004/src/main/java/com/test003/eurakeserver004/Eurakeserver004Application.java
@@ -0,0 +1,13 @@
+package com.test003.eurakeserver004;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Eurakeserver004Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Eurakeserver004Application.class, args);
+    }
+
+}
---
+++ src/main/java/com/test003/eurakeserver002/EurakeServer002Application.java
@@ -0,0 +1,13 @@
+package com.test003.eurakeserver002;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EurakeServer002Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EurakeServer002Application.class, args);
+    }
+
+}
